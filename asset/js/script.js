b4d19fb826676891759c8fee0c99ace694879061
Sugar Shane <scrisostomo17@gmail.com>
2021-06-19 11:56:43 -0400

    got the questions to show up

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,71 +1,152 @@
 var questions= [
       { questions: `What is THE `,  
       choices: ["Shane", "Shaun", "Shawn", "Shain"], 
-      answer: 'b'},
+      answer: 'Shane'},
 
       { question: `What is my age`,  
-      choices: ["Shane", "Shaun", "Shawn", "Shain"], 
-      answer: 'b'},
+      choices: ["1", "S2", "3", "4"], 
+      answer: 'S2'},
 
       { questions: `What is my last name`,  
-      choices: ["Shane", "Shaun", "Shawn", "Shain"], 
-      answer: 'b'},
+      choices: ["Shane", "Crisostomo", "Shawn", "Shain"], 
+      answer: 'Crisostomo'},
 
       { questions: `What is my middle name`,  
-      choices: ["Shane", "Shaun", "Shawn", "Shain"], 
-      answer: 'b'},
+      choices: ["Shane", "Hello", "Shawn", "Shain"], 
+      answer: 'Hello'},
 
       { questions: `What is my father's name`,  
-      choices: ["Shane", "Shaun", "Shawn", "Shain"], 
-      answer: 'b'},
+      choices: ["Shane", "Dad", "Shawn", "Shain"], 
+      answer: 'Dad'},
 
       { questions: `What is my sister's name`,  
-      choices: ["Shane", "Shaun", "Shawn", "Shain"], 
-      answer: 'b'},
+      choices: ["Shane", "Sister", "Shawn", "Shain"], 
+      answer: 'Sister'},
 
-      { questions: `What is my nother's name`,  
-      choices: ["Shane", "Shaun", "Shawn", "Shain"], 
+      { questions: `What is my mother's name`,  
+      choices: ["Mom", "Shane", "Shawn", "Shain"], 
       answer: 'b'}
 ];
 
+
 var score = 0;
-var userAnswer = "";
-var timeLeft = 15;
-var questionsContainer = document.getElementById('questions')
-var submitButton = document.getElementById('submit');
+var startButtonEl = document.getElementById("startButton")
+
+//start game button - when the button is click the timer starts
+// document.getElementById("startButton").addEventListener('click', function (){
+//     startButtonEl.disabled = true;
+//   var timeleft = 15
+
+//     var downloadTimer = setInterval(function function1(){
+//         document.getElementById("countdownTimer").innerHTML = timeleft + 
+//         "&nbsp"+"seconds remaining!";
+    
+//         timeleft -= 1;
+//         if(timeleft <= 0){
+//             clearInterval(downloadTimer);
+//             document.getElementById("countdownTimer").innerHTML = "Time is up!!!"
+//             displayQuestion();
+//         }
+//         }, 1000);
+    
+//         displayQuestion();
+//     }); 
+
+// when start button is clicked the game starts - first question is displayed
+function displayQuestion() {
+    console.log("question")
+  for (let index = 0; index < questions.length; index++) {
+      document.getElementById("questionSelection").innerHTML = questions[index].questions;
+      document.getElementById("btnA").innerHTML = questions[index].choices[0];
+      document.getElementById("btnB").innerHTML = questions[index].choices[1];
+      document.getElementById("btnC").innerHTML = questions[index].choices[2];
+      document.getElementById("btnD").innerHTML = questions[index].choices[3];
+
+      // //listen for the click in the questionsContainer then if the answer is = correct then 
+      // var answerClick = document.getElementById("answerButtons")
+
+      // console.log(answerClick)
+
+      // answerClick.addEventListener('click', function()s{
+
+      //   if (
+      //     (answer === true && questions[index].answer === 't') ||
+      //     (answer === false && questions[index].answer === 'f')
+      //   ) {
+      //     // Increase score
+      //     score++;
+      //     // Tell the user what their answer status is
+      //     document.getElementById("answerStatus").innerHTML = questions[index].answer
+
+          
+      //   } else {
+      //     alert('Wrong!');
+      //   }
+
+      // });
 
 
-//start game button
+  }
+      
 
-//when start button is clicked the game starts
+      
+
+     
+};
+
+//then the start button game disappears when game is started
+// function hideStartButton() {
+//     var startButtonEl = document.getElementById("startGame");
+//     if (startButtonEl.style.display === "none") {
+//         startButtonEl.style.display = "block";
+//     } else {
+//         startButtonEl.style.display = "none";
+//     }
+   
+// };
+
+
+
+
+//when timer runs out, then continue to next question
+
+//parse through random questions by displaying them
+
+//parse through answer
+
+//correct answer turns green background
+
+//wrong answer turns red background
+
+//when you go through all the questions then the game is over
+
+//display total score
+
+//save initials and score for high score 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //when the start button is clicked then the timer starts
-    var seconds = document.getElementById("countdown").textContent;
-    var countdownTimer = setInterval(function() {
-       seconds--;
-       document.getElementById("countdown").textContent = seconds;
-       //when timer reaches 0 then the user is alerted and then user is alerted
-       if (seconds <= 0) clearInterval(countdownTimer);
-       
-    }, 1000);
+    // var seconds = document.getElementById("countdown").textContent;
+    // var countdownTimer = setInterval(function() {
+    //    seconds--;
+    //    document.getElementById("countdown").textContent = seconds;
+    //    //when timer reaches 0 then the user is alerted and then user is alerted
+    //    if (seconds <= 0) clearInterval(countdownTimer);
+
+    // }, 1000);
  
-
-//star to build the quick
-//parse through array questions by displaying them
-
-// for (var i = 0; i < questions.length)
-
-
-//show results
-
-
-
-
-
-
-
-
-
-
-
-
